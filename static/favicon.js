4e5eac05b65d91c072441f751cb3a3a800818985
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2023-10-16 16:20:44 +0000

    zzz

--- static/favicon.js
+++ static/favicon.js
@@ -5,7 +5,28 @@ function changeIcon(){
   if(icon){
     icon.href = "https://raw.githubusercontent.com/Patrick-ring-motive/Async-Python-Reverse-Proxy/main/static/static/favicon.png";
     icon.setAttribute('touched','true');
+  }else if(!document.querySelector('link[rel*="icon"]')){
+    let link = document.createElement('link');
+    link.rel = 'icon';
+    link.href = 'https://raw.githubusercontent.com/Patrick-ring-motive/Async-Python-Reverse-Proxy/main/static/static/favicon.png';
+    link.setAttribute('touched','true');
+    document.head.appendChild(link);
   }
 }
 
 setInterval(function(){changeIcon();},100);
+
+
+document.addEventListener("readystatechange", (event) => {
+  changeIcon();
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  changeIcon();
+});
+
+document.addEventListener("load", (event) => {
+  changeIcon();
+});
+
+changeIcon();
