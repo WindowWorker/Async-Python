7ac3cc7e64dbace684815ffb1bcc20b57f5f3c5b
WindowWorker <122478085+WindowWorker@users.noreply.github.com>
2024-03-13 16:29:09 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -22,18 +22,19 @@ globalThis.staticPrefix = 'https://raw.githubusercontent.com/Patrick-ring-motive
 
 globalThis.hostTargetList = [
   'www.python.org',
-  'packaging-python-org.vercel.app',
-  'docs-python-org.vercel.app',
-  'pypi.org',
-  'www-pypa-io.vercel.app',
-  'wiki.python.org',
-  'peps.python.org',
-  'mail.python.org',
-  'bugs-python-org.vercel.app',
-  'discuss.python.org',
-  'devguide-python-org.vercel.app',
-  'planetpython.org',
-  'pyfound.blogspot.com'
+  'www.python.org'
+ # 'packaging-python-org.vercel.app',
+ # 'docs-python-org.vercel.app',
+ # 'pypi.org',
+ # 'www-pypa-io.vercel.app',
+ # 'wiki.python.org',
+ # 'peps.python.org',
+ # 'mail.python.org',
+ # 'bugs-python-org.vercel.app',
+ # 'discuss.python.org',
+ # 'devguide-python-org.vercel.app',
+ # 'planetpython.org',
+ # 'pyfound.blogspot.com'
 ]
 globalThis.hostShortCircuit = ['planetpython.org']
 
